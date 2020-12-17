a5fb04b3cc6f9248d3997c077b49cae5d0899e31
Alexis Jimenex <ijimenez@pagando.mx>
2020-12-17 11:08:56 -0700

    Initial Commit

---
+++ face-gesture-detection-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  face-gesture-detection-demo
+//
+//  Created by Alexis Jimenez on 17/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
